a31361985792531caab0fde0807f8c80f6cdae55
zb <zinabubirhanu3535@gmail.com>
2024-02-28 22:10:20 +0300

    personal portofolio

---
+++ react-redux-app/personal-portfolio/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li><a href="#!">About</a></li>
+          <li><a href="#!">Works</a></li>
+          <li><a href="#!">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
